a0935db795e98b6f337f3671c321d3a5e7f1faf4
AMAN JAIMAN <77478827+amanjaiman1@users.noreply.github.com>
2023-11-09 23:52:42 +0530

    Add: some changes updated

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -88,6 +88,6 @@
         </div>
     </div>
     <p class="w-25 w-4/5 pb-3 m-auto text-xs text-gray-100 pt-6">
-        Copyright 2017-2021 Code With Dary. All Rights Reserved
+        Code with ❤️ by Aman Jaiman
     </p>
 </footer>
